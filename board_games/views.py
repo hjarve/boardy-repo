30d9d7994dbdd3c2f93eb605d0bf931d64828494
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2021-12-01 12:01:56 +0200

    started users app, created forms, created url pattern for login page and registering page, created a view function for registration page, created url pattern and view function of rindex page

--- board_games/views.py
+++ board_games/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """The home page for Boardy"""
+    return render(request, 'board_games/index.html')
--- boardy/settings.py
+++ boardy/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'board_games',
+    'users',
 
     # Default django apps.
     'django.contrib.admin',
