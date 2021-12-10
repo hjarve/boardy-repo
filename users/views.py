0f0a2d921faa620524bcc828f64659712caa4e69
Hanna Järveläinen <hanna.jarvelainen@hotmail.com>
2021-12-10 19:51:15 +0200

    debugging users views

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
         # Process completed form.
         form = UserCreationForm(data = request.POST)
 
-        if form.is_valid:
+        if form.is_valid():
             new_user = form.save()
             # Log the user in and then redirect to home page.
             login(request, new_user)
